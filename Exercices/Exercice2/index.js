049ae51325777a611c1e31dd6c0ed11ab1aa1e78
Quentin ANIERE <quentin.aniere@gmail.com>
2021-12-21 23:30:14 +0100

    feat: improved anchors by using JS

--- Exercices/Exercice2/index.js
+++ Exercices/Exercice2/index.js
@@ -1,6 +1,9 @@
+const regex = new RegExp("#(.*)");
+
 var menuToggled = false;
 var main = document.querySelector("main");
 var navbar = document.querySelector("nav");
+var links = document.querySelectorAll("a");
 var banner = document.getElementById("banner");
 var mobileMenu = document.getElementById("mobile-menu");
 var button = document.getElementById("mobile-menu-icon");
@@ -17,6 +20,7 @@ window.onscroll = ( () => {
         navbar.classList.remove("sticky");
         main.classList.remove("add-padding");
       }
+
 });
 
 button.addEventListener("click", () => {
@@ -33,4 +37,24 @@ button.addEventListener("click", () => {
         navbar.classList.add("nav-larger");
         mobileMenu.style.display = "block";
     }
+});
+
+links.forEach(link => {
+    link.addEventListener("click", (element) => {
+
+        element.preventDefault();
+        let completeLink = element.path[0].href;
+        let destination = regex.exec(completeLink)[1];
+
+        if(destination === "") {
+            window.scrollTo(0, 0);
+
+        } else {
+            
+            let title = document.getElementById(destination);
+            let pad = window.innerHeight / 9;
+            console.log(pad);
+            window.scrollTo(0, title.offsetTop - pad);
+        }
+    });
 });
