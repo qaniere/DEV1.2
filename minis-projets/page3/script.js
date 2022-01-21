77fafc1dc215f8ffcaa93a919370a7bcc19d6b58
= <=>
2022-01-21 19:07:30 +0100

    feat: cleaned the code

--- minis-projets/page3/script.js
+++ minis-projets/page3/script.js
@@ -22,6 +22,14 @@ CANVAS.setAttribute("width", widthAfterStyle * DPI);
 //in order to prevent blurry draws.
 
 
+/**
+ * This function draw a circle in the canvas 
+ *  
+ * @param {int} x The x coordinate of the circle - Must be positive
+ * @param {int} y The y coordinate of the circle - Must be positive
+ * @param {int} radius The radius of the circle - Must be positive
+ * @param {string} color The color of the cirle - Must be a valid css value
+ */
 function drawCircle(x, y, radius, color) {
 
     CTX.beginPath();
@@ -36,6 +44,9 @@ function drawCircle(x, y, radius, color) {
     DOWNLOAD_LINK.href = canvasData;
 }
 
+/**
+ * This function returns a random hex CSS color
+ */
 function getRandomHexColor() {
     let randomInt = Math.floor(Math.random() * (9999999 - 0 + 1) + 0);
     //This calculation return's a random int of nine digits
@@ -46,6 +57,7 @@ function getRandomHexColor() {
     return hexString;
 }
 
+//This callback is executed when the canvas is clicked
 CANVAS.addEventListener("mousedown", (event) => {
 
     let realCanvas = CANVAS.getBoundingClientRect(); //This method return the real height and width of the canvas
@@ -57,6 +69,7 @@ CANVAS.addEventListener("mousedown", (event) => {
     drawCircle(x, y, radius, getRandomHexColor());
 });
  
+//This callback is executed when the the mouse whell is spinning
 document.addEventListener("wheel", (event) => {
 
     if(event.deltaY < 0) {
@@ -72,6 +85,7 @@ document.addEventListener("wheel", (event) => {
     RADIUS_DISPLAY.innerText = "Taille du cercle : " + radius + "px";
 });
 
+//This callback is executed when the clear button is clicked
 CLEAR_BUTTON.addEventListener("click", (event) => {
 
     let userConfirmation = confirm("Voulez-vous vraiment effacer la toile ?");
@@ -81,15 +95,22 @@ CLEAR_BUTTON.addEventListener("click", (event) => {
     }    
 });
 
+//This callback is executed when the download button is clicked
 DOWNLOAD_BUTTON.addEventListener("click", (event) => {
     DOWNLOAD_LINK.click();
+    //To download the canvas content, we need the href and the download attributes
+    //Since they can't apply to a button and wrap the button arround a <a> is a
+    //bad practice,  a <a> element with the href and download attributes is hided
+    //on the page and triggered when the button is clicked.
 });
 
+//This callback is executed when the plus button is clicked (Mobile devices only)
 PLUS_BUTTON.addEventListener("click", () => {
     radius = radius + 10;
     RADIUS_DISPLAY.innerText = "Taille du cercle : " + radius + "px";
 });
 
+//This callback is executed when the minus button is clicked (Mobile devices only)
 MINUS_BUTTON.addEventListener("click", () => {
     if(radius != 0) {
         radius = radius - 10;
@@ -97,6 +118,10 @@ MINUS_BUTTON.addEventListener("click", () => {
     }
 });
 
+//This callback is executed when the document is double clicked
 document.addEventListener("dblclick", (event) => {
-    EventSource.preventDefault();
+    event.preventDefault();
+    //On computer devices, this does nothing but on mobile devices
+    //it prevent automatic zoom that can't be really anoying for
+    //user
 });
